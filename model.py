8652354279c583805c3fc2bfc90af1a43d6e7df2
Thanh-Dat Truong <thanhdattrg@gmail.com>
2018-04-19 23:43:49 +0700

    Model

--- model.py
+++ model.py
@@ -1,121 +1,117 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import numpy as np
+import sys
+
+FLOAT_TYPE = tf.float32
 
 class DCTI:
-    def __init__(self, _num_class=10, _learning_rate=0.001, _regularization_rate=0.0005, _batch_size=32, _num_epoch=200):
-        self.num_class = _num_class
-        self.batch_size = _batch_size
-        self.num_epoch = _num_epoch
-        self.learning_rate = _learning_rate
-        self.regularization_rate = _regularization_rate
-        self.net = self.build_network()
-        self.trainer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
+    def __init__(self, num_class=10, learning_rate=0.001, regularization_rate=0.0005, batch_size=256, num_epoch=200, logdir='logs'):
+        self.num_class = num_class
+        self.batch_size = batch_size
+        self.num_epoch = num_epoch
+        self.learning_rate = learning_rate
+        self.regularization_rate = regularization_rate
+        self.use_regularization = False if self.regularization_rate is None else True
+
+        self.images = tf.placeholder(shape=[None, 32, 32, 3], dtype=FLOAT_TYPE)
+        self.labels = tf.placeholder(shape=[None, 10], dtype=FLOAT_TYPE)
+
+        #self.weights_initializer=tf.truncated_normal_initializer(0.0, 0.01)
+
+        if not self.use_regularization:
+            self.net=  self.build_network()
+        else:
+            self.regularizer = slim.l2_regularizer(self.regularization_rate)
+            with slim.arg_scope([slim.conv2d, slim.fully_connected], \
+                    weights_regularizer=self.regularizer):
+                self.net = self.build_network()
+
+        self.logdir = logdir
 
     def build_network(self):
-        self.input = tf.placeholder(shape=[None, 32, 32, 3], dtype=tf.float32)
-        self.label = tf.placeholder(shape=[None, self.num_class], dtype=tf.float32)
-        self.regularizer = slim.l2_regularizer(self.regularization_rate)
-
         # First Phase 32x32
-        net = slim.conv2d(self.input, 64, [3, 3], scope='conv1_1',  weights_regularizer=self.regularizer)
-        net = slim.conv2d(net, 64, [3, 3], scope='conv1_2', weights_regularizer=self.regularizer)
+        net = slim.conv2d(self.images, 64, [3, 3], scope='conv1_1')
+        net = slim.conv2d(net, 64, [3, 3], scope='conv1_2')
         net = slim.max_pool2d(net, [2, 2], scope='pool1')
+
         # Second Phase 16x16
-        net = slim.conv2d(net, 128, [3, 3], scope='conv2_1', weights_regularizer=self.regularizer)
-        net = slim.conv2d(net, 128, [3, 3], scope='conv2_2', weights_regularizer=self.regularizer)
+        net = slim.conv2d(net, 128, [3, 3], scope='conv2_1')
+        net = slim.conv2d(net, 128, [3, 3], scope='conv2_2')
         net = slim.max_pool2d(net, [2, 2], scope='pool2')
+
         # Third Phase 8x8
-        net = slim.conv2d(net, 256, [3, 3], scope='conv3_1', weights_regularizer=self.regularizer)
-        net = slim.conv2d(net, 256, [3, 3], scope='conv3_2', weights_regularizer=self.regularizer)
-        net = slim.conv2d(net, 256, [3, 3], scope='conv3_3', weights_regularizer=self.regularizer)
+        net = slim.conv2d(net, 256, [3, 3], scope='conv3_1')
+        net = slim.conv2d(net, 256, [3, 3], scope='conv3_2')
+        net = slim.conv2d(net, 256, [3, 3], scope='conv3_3')
         net = slim.max_pool2d(net, [2, 2], scope='pool3')
+
         #Fourth Phase 4x4
-        net = slim.conv2d(net, 512, [3, 3], scope='conv4_1', weights_regularizer=self.regularizer)
-        net = slim.conv2d(net, 512, [3, 3], scope='conv4_2', weights_regularizer=self.regularizer)
+        net = slim.conv2d(net, 512, [3, 3], scope='conv4_1')
+        net = slim.conv2d(net, 512, [3, 3], scope='conv4_2')
         net = slim.max_pool2d(net, [2, 2], scope='pool4')
+
         #Fifth Phase 2x2
-        net = slim.conv2d(net, 512, [3, 3], scope='conv5_1', weights_regularizer=self.regularizer)
+        net = slim.conv2d(net, 512, [3, 3], scope='conv5_1')
         net = slim.avg_pool2d(net, [2, 2], scope='pool5')
 
         # Fully Connected
         net = slim.flatten(net, scope='flatten')
-        net = slim.fully_connected(net, self.num_class, activation_fn=None, scope='fc', weights_regularizer=self.regularizer)
+        net = slim.fully_connected(net, self.num_class, activation_fn=None, scope='fc')
 
-        #Loss function
-        self.softmax_loss = slim.losses.softmax_cross_entropy(net, self.label)
-        self.loss =  slim.losses.get_total_loss(add_regularization_losses=False)
-        self.accuracy = tf.metrics.accuracy(labels=tf.argmax(self.label, axis=1), predictions=tf.argmax(net, axis=1))
+        return net
 
-
-    def forward(self, sess, images):
-        predict = sess.run(self.net, feed_dict = {self.input: images})
-        return predict
 
     def backward(self, sess, images, labels):
-        _, summary, loss, accuracy = sess.run([self.trainer, self.summary_op, self.loss, self.accuracy], feed_dict={self.input: images, self.label: labels})
+        _, loss, accuracy, summary = sess.run([self.optimizer, self.loss, self.accuracy, self.summary_op], feed_dict={self.images: images, self.labels: labels})
+        self.counter = self.counter + 1
         self.summary_writer.add_summary(summary, self.counter)
         return loss, accuracy
 
-    def train_epoch(self, sess, images, labels):
-        length = len(images)
+    def epoch(self, sess, images, labels):
+        total_loss = 0.0
+        total_accuracy = 0.0
         first = 0
-        loss = 0.0
-        accuracy = 0.0
+        length = len(images)
         iter = 0
         while (first < length):
-            iter = iter + 1
-            self.counter = self.counter + 1
-            last = first + self.batch_size
-            if (last > length):
-                last = length
-            l, a = self.backward(sess, images[first:last], labels[first:last])
-            loss = loss + l * (last - first)
-            accuracy = accuracy + a[0] * (last - first)
-            print '\tIteration %d: Loss: %f Accuracy %f' % (iter, l, a[0] * 100) + '%'
+            last = min(length, first + self.batch_size)
+            loss, accuracy = self.backward(sess, images[first:last], labels[first:last])
+            total_loss = total_loss + loss * (last - first)
+            total_accuracy = total_accuracy + accuracy * (last - first)
             first = last
+            iter = iter + 1
+            sys.stdout.write("\033[F") # Cursor up one line
+            print '\tIteration %d: Loss: %.5f Accuracy: %.2f' % (iter, loss, accuracy * 100) + '%'
 
-        return loss / float(length), accuracy / float(length)
+        return total_loss / float(length), total_accuracy / float(length)
 
     def train(self, sess, images, labels):
+        self.softmax_loss = slim.losses.softmax_cross_entropy(self.net, self.labels)
+        if self.use_regularization:
+            self.regularization_loss = tf.add_n(slim.losses.get_regularization_losses())
+        self.loss = slim.losses.get_total_loss()
+        #self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.net, labels=self.labels))
+
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.net, axis=1), tf.argmax(self.labels, axis=1)), FLOAT_TYPE))
 
         tf.summary.scalar("Loss", self.loss)
-        tf.summary.tensor_summary("Accuracy", self.accuracy)
+        tf.summary.scalar("Accuracy", self.accuracy)
         self.summary_op = tf.summary.merge_all()
-        self.summary_writer = tf.summary.FileWriter('logs/', graph=tf.get_default_graph())
-
-        saver = tf.train.Saver()
+        self.summary_writer = tf.summary.FileWriter(self.logdir, graph=tf.get_default_graph())
         self.counter = 0
-        for i in xrange(1, self.num_epoch + 1):
-            loss, accuracy = self.train_epoch(sess, images, labels)
-            print 'Epoch %d: Loss: %f Accuracy %f' % (i, loss, accuracy * 100) + '%'
-            saver.save(sess, 'output/cifar_%d_model' % self.num_class)
+        saver = tf.train.Saver()
 
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
 
-def parse_data_cifar10():
-    import pickle
-    data = []
-    labels = []
-    for i in xrange(1, 6):
-        dict = pickle.load(open('data/data_batch_%d' % i, 'rb'))
-        length = len(dict['labels'])
-        arr = np.array(dict['data'], dtype=np.float32) / 255
-        lab = np.zeros((length, 10), dtype=np.float32)
-        lab[np.arange(length), np.array(dict['labels'], dtype=np.int)] = 1.0
-        data.append(arr)
-        labels.append(lab)
-    data = np.vstack(data)
-    data = data.reshape((-1, 32, 32, 3))
-    labels = np.vstack(labels)
-    return data, labels
+        for iter in xrange(1, self.num_epoch + 1):
+            print 'Epoch %d\n' % iter
+            loss, accuracy = self.epoch(sess, images, labels)
+            print '\tResult: Loss: %.5f Accuracy: %.2f' % (loss, accuracy * 100) + '%'
+            saver.save(sess, 'model/model', global_step=iter)
 
-if __name__ == '__main__':
-    data, labels = parse_data_cifar10()
-    net = DCTI(_batch_size=256)
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
-    net.train(sess, data, labels)
 
 
 
