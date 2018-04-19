25fdb11dc82a8fc436e61c96c48bcb308e416d17
Thanh-Dat Truong <thanhdattrg@gmail.com>
2018-04-19 23:43:30 +0700

    Training

---
+++ train.py
@@ -0,0 +1,10 @@
+import model
+import dataset
+
+import tensorflow as tf
+if __name__ == '__main__':
+    images, labels = dataset.parse_data_cifar10()
+    images, labels = dataset.preprocess_image(images, labels, num_class=10)
+    sess = tf.Session()
+    net = model.DCTI(learning_rate=0.01, batch_size=256, regularization_rate=0.0005)
+    net.train(sess, images, labels)
